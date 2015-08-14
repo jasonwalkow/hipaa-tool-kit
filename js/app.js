36154f5ec4cb711765b8835931de67450ad8c872
Jason Walkow <jason.walkow1@gmail.com>
2015-08-14 15:34:14 -0700

    commit updates

--- js/app.js
+++ js/app.js
@@ -1,33 +1,12 @@
-/* FSVS SLIDER
-$(document).ready( function() {
-	var fsvs = $.fn.fsvs({
-		speed : 5000,
-		bodyID : 'fsvs-body',
-		selector : '> .slide',
-		mouseSwipeDisance : 40,
-		afterSlide : function(){},
-		beforeSlide : function(){},
-		endSlide : function(){},
-		mouseWheelEvents : true,
-		mouseDragEvents : true,
-		touchEvents : true,
-		arrowKeyEvents : true,
-		pagination : true,
-		nthClasses : false,
-		detectHash : true
-	});
-});
 
-$(document).ready( function() {
-	var slider = $.fn.fsvs({
-		...
-		endSlide : function(index) {
-			$('<div class="slide"><h2>Slide ' + (index+2) + '</h2></div>').appendTo( $('body') );
-			// reset the nth classes if you need it
-			slider.nthClasses(3);
-			// rebind pagination if you need it
-			slider.addPagination();
-		}
-		...
-	});
-});*/
+$(document).ready(function() {
+	function scrollToAnchor(aid){
+		var aTag = $("a[name='"+ aid +"']");
+		$('html,body').animate({scrollTop: aTag.offset().top},'slow');
+	}
+	$("a").click(function(event) {
+		event.preventDefault();
+		var href = $(this).attr('href').replace('#', '')
+		scrollToAnchor(href);
+	});			
+});
